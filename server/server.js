55e0e7f2480a14bd424c3f4f85d957272ac7e091
sanskardrolia <sanskardrolia@gmail.com>
2025-04-29 21:14:58 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
